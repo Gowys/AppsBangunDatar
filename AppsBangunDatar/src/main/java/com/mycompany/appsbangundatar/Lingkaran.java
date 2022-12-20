d86d6b30a3ac72b0e30a1d0678adbb0fabf02ea3
rosaat23 <rosa.anugrah@mhs.itenas.ac.id>
2022-12-20 12:39:25 +0700

    add feature lingkaran

--- AppsBangunDatar/src/main/java/com/mycompany/appsbangundatar/Lingkaran.java
+++ AppsBangunDatar/src/main/java/com/mycompany/appsbangundatar/Lingkaran.java
@@ -8,6 +8,48 @@ package com.mycompany.appsbangundatar;
  *
  * @author ACER
  */
-public class Lingkaran {
+public class Lingkaran extends BangunDatar{
     
+    public final double PHI = 3.14;
+    double r;
+
+    public Lingkaran() {
+    }
+
+    public Lingkaran(double r) {
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    @Override
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        super.setNamaBangunDatar(namaBangunDatar); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String getNamaBangunDatar() {
+        return super.getNamaBangunDatar(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+    
+    
+
+    @Override
+    double luas() {
+        return PHI * r * r;
+    }
+
+    @Override
+    double keliling() {
+    
+        return 2 * PHI *r;
+    
+}
 }
