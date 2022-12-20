6c9a9856f2015f5c7e727ead23b9899f4e73b376
Hugo <hgabigael87@gmail.com>
2022-12-20 12:13:42 +0700

    add feauture segitiga

---
+++ AppsBangunDatar/src/main/java/com/mycompany/appsbangundatar/Segitiga.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.appsbangundatar;
+
+/**
+ *
+ * @author dell
+ */
+public class Segitiga extends BangunDatar{
+    private double alas;
+    private double tinggi;
+
+    public Segitiga() {
+    }
+
+    public double getAlas() {
+        return alas;
+    }
+
+    public void setAlas(double alas) {
+        this.alas = alas;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+    
+    
+
+    @Override
+    double luas() {
+        return 0.5 * this.alas * this.tinggi;
+    }
+
+    @Override
+    double keliling() {
+        return this.alas + tinggi + getSisiMiring();
+    }
+    
+    //Overload
+    public float luas(float alas, float tinggi){
+        return 0.5 * (this.alas * this.tinggi);
+    }
+    
+    //Overload
+    public float keliling(float alas, float tinggi){
+        return this.alas + this.tinggi + getSisiMiring();
+    }
+    
+    public float getSisiMiring(){
+        return (float) Math.sqrt((this.alas*this.alas)+(this.tinggi*this.tinggi));
+    }
+    
+}
